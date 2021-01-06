c4250eeb4652c5a8556a987dbdb7431c220f3c3d
SvetlinNikolov <daxteranikolov@gmail.com>
2021-01-06 12:22:00 +0200

    Initial commit

---
+++ Assets/Scripts/GlobalEuro.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalEuro : MonoBehaviour
+{
+    public static ulong EuroCount { get; set; }
+    public static float Multiplier { get; set; } = 0.0F;
+
+    public GameObject EuroDisplay;
+
+    void Update()
+    {
+        EuroDisplay.GetComponent<Text>().text = "Euro: " + EuroCount;
+    }
+}
---
+++ Assets/Scripts/GlobalEuroClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GlobalEuroClick : MonoBehaviour
+    {
+        public GameObject textBox;
+        public void EuroOnClicked()
+        {
+            GlobalEuro.EuroCount += (ulong)(GlobalEuro.EuroCount * GlobalEuro.Multiplier + 1);
+            textBox.SetActive(true);
+        }
+    }
+}
